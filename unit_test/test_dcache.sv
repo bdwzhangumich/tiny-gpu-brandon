bf210dc72ef6a63f669517e7505d467b98bc90ce
Brandon Zhang <bdwzhang@umich.edu>
2024-07-05 02:06:16 -0500

    Split inputs and outputs into separate interfaces and added parameters

--- unit_test/test_dcache.sv
+++ unit_test/test_dcache.sv
@@ -13,7 +13,26 @@ module testbench #(
 	reg clk;
 	
 	always #5 clk =~ clk;
-    dcache_if _if(clk);
+    dcache_input_if #(
+        .ADDR_BITS(ADDR_BITS),
+        .DATA_BITS(DATA_BITS),
+        .NUM_CONSUMERS(NUM_CONSUMERS),
+        .NUM_CHANNELS(NUM_CHANNELS),
+        .NUM_BLOCKS(NUM_BLOCKS),
+        .NUM_BANKS(NUM_BANKS),
+        .NUM_WAYS(NUM_WAYS),
+        .CACHE_BLOCK_SIZE(CACHE_BLOCK_SIZE)
+    ) in_if(clk);
+    dcache_output_if #(
+        .ADDR_BITS(ADDR_BITS),
+        .DATA_BITS(DATA_BITS),
+        .NUM_CONSUMERS(NUM_CONSUMERS),
+        .NUM_CHANNELS(NUM_CHANNELS),
+        .NUM_BLOCKS(NUM_BLOCKS),
+        .NUM_BANKS(NUM_BANKS),
+        .NUM_WAYS(NUM_WAYS),
+        .CACHE_BLOCK_SIZE(CACHE_BLOCK_SIZE)
+    ) out_if(clk);
 	dcache #(
         .ADDR_BITS(ADDR_BITS),
         .DATA_BITS(DATA_BITS),
@@ -27,36 +46,45 @@ module testbench #(
         .clk(clk),
         .reset(reset),
 
-        .consumer_read_valid(_if.consumer_read_valid),
-        .consumer_read_address(_if.consumer_read_address),
-        .consumer_read_ready(_if.consumer_read_ready),
-        .consumer_read_data(_if.consumer_read_data),
-        .consumer_write_valid(_if.consumer_write_valid),
-        .consumer_write_address(_if.consumer_write_address),
-        .consumer_write_data(_if.consumer_write_data),
-        .consumer_write_ready(_if.consumer_write_ready),
+        .consumer_read_valid(in_if.consumer_read_valid),
+        .consumer_read_address(in_if.consumer_read_address),
+        .consumer_read_ready(out_if.consumer_read_ready),
+        .consumer_read_data(out_if.consumer_read_data),
+        .consumer_write_valid(in_if.consumer_write_valid),
+        .consumer_write_address(in_if.consumer_write_address),
+        .consumer_write_data(in_if.consumer_write_data),
+        .consumer_write_ready(out_if.consumer_write_ready),
 
-        .controller_read_valid(_if.controller_read_valid),
-        .controller_read_address(_if.controller_read_address),
-        .controller_read_ready(_if.controller_read_ready),
-        .controller_read_data(_if.controller_read_data),
-        .controller_write_valid(_if.controller_write_valid),
-        .controller_write_address(_if.controller_write_address),
-        .controller_write_data(_if.controller_write_data),
-        .controller_write_ready(_if.controller_write_ready)
+        .controller_read_valid(out_if.controller_read_valid),
+        .controller_read_address(out_if.controller_read_address),
+        .controller_read_ready(in_if.controller_read_ready),
+        .controller_read_data(in_if.controller_read_data),
+        .controller_write_valid(out_if.controller_write_valid),
+        .controller_write_address(out_if.controller_write_address),
+        .controller_write_data(out_if.controller_write_data),
+        .controller_write_ready(in_if.controller_write_ready)
     );
 
 	initial begin
 		clk = 0;
 		// TODO: properly separate inputs and outputs
 		@(negedge clk);
-		$display("controller_read_valid=0x%h",_if.controller_read_valid);
+		$display("controller_read_valid=0x%h",out_if.controller_read_valid);
 		$finish;
 	end
 
 endmodule
 
-interface dcache_if (input clk);
+interface dcache_input_if #(
+	parameter ADDR_BITS = 8,
+	parameter DATA_BITS = 8,
+	parameter NUM_CONSUMERS = 8,
+	parameter NUM_CHANNELS = 8,
+	parameter NUM_BLOCKS = 8,
+	parameter NUM_BANKS = 2,
+	parameter NUM_WAYS = 4,
+	parameter CACHE_BLOCK_SIZE = 1,
+) (input clk);
     // Consumer Inputs
     logic [NUM_CONSUMERS-1:0] consumer_read_valid;
     logic [ADDR_BITS-1:0] consumer_read_address [NUM_CONSUMERS-1:0];
@@ -68,7 +96,18 @@ interface dcache_if (input clk);
     logic [NUM_CONSUMERS-1:0] controller_read_ready;
     logic [DATA_BITS-1:0] controller_read_data [NUM_CONSUMERS-1:0];
     logic [NUM_CONSUMERS-1:0] controller_write_ready;
+endinterface
 
+interface dcache_output_if #(
+	parameter ADDR_BITS = 8,
+	parameter DATA_BITS = 8,
+	parameter NUM_CONSUMERS = 8,
+	parameter NUM_CHANNELS = 8,
+	parameter NUM_BLOCKS = 8,
+	parameter NUM_BANKS = 2,
+	parameter NUM_WAYS = 4,
+	parameter CACHE_BLOCK_SIZE = 1,
+) (input clk);
     // Consumer Outputs
     logic [NUM_CONSUMERS-1:0] consumer_read_ready;
     logic [DATA_BITS-1:0] consumer_read_data [NUM_CONSUMERS-1:0];
